26c8ba0697afc3912f186982189c64009a6a0517
Bruno Zuada Coelho <brunozuada@gmail.com>
2020-06-09 15:17:48 +0200

    added hash key to file

--- wolfStiffness.py
+++ wolfStiffness.py
@@ -1,10 +1,13 @@
+import numpy as np
+import os
+import LayeredHalfSpace
+
+
 def wolf_stiffness(layer_file, omega, freq=False):
     """Dynamic stiffness according to Wolf and Deeks
        Layered soil solution
        Only considers the translational cones. The rotational cones are not considered
        """
-
-    import LayeredHalfSpace
 
     layers = LayeredHalfSpace.read_file(layer_file)
 
@@ -21,5 +24,7 @@ def wolf_stiffness(layer_file, omega, freq=False):
 
 
 if __name__ == "__main__":
-    import numpy as np
-    wolf_stiffness("./example/input.csv", np.linspace(0, 750, 50))
+    # creates files with the hash version of the git
+    os.system(r"git describe --all --long > version.txt")
+    # runs stiffness
+    wolf_stiffness("./example/input.csv", np.linspace(0, 500, 50))
