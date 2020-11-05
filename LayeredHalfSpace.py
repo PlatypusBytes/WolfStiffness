added comparison with the results of Wolf fixed damping computation

LayeredHalfSpace.py
@@ -237,11 +237,10 @@ def write_output(path, data, omega, freq):
             f.write(str(omega[i]) + ";" +
                     str(data.K_dyn[i]) + ";" +
                     str(np.real(data.K_dyn[i])) + ";" +
-                    str(np.imag(data.K_dyn[i] * data.radius / data.cs[1]) / omega[i]) + "\n")
+                    str(np.imag(data.K_dyn[i]) / (omega[i] * data.radius / np.real(data.cs[1]))) + "\n")
             # add to tmp variables
-            stiff_tmp.append(np.abs(np.real(data.K_dyn[i])))
-            damp_tmp.append(np.abs(np.imag(data.K_dyn[i] * data.radius / data.cs[1]) / omega[i]))
-
+            stiff_tmp.append(np.real(data.K_dyn[i]))
+            damp_tmp.append(np.imag(data.K_dyn[i]) / (omega[i] * data.radius / np.real(data.cs[1])))
 
     # make plots
     if freq:

wolfStiffness.py
@@ -27,4 +27,4 @@ if __name__ == "__main__":
     # creates files with the hash version of the git
     os.system(r"git describe --all --long > version.txt")
     # runs stiffness
-    wolf_stiffness("./example/input.csv", np.linspace(0, 500, 50))
+    wolf_stiffness("./example/input.csv", np.linspace(0, 5, 50))