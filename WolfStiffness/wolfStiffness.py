8b7aef412a5db1efd72fd63316468fff0cb60ad6
Bruno Zuada Coelho <brunozuada@gmail.com>
2021-10-27 16:52:58 +0200

    added comments to code

--- WolfStiffness/wolfStiffness.py
+++ WolfStiffness/wolfStiffness.py
@@ -9,23 +9,42 @@ class WolfStiffness:
     Layered soil solution
     Only considers the translational cones. The rotational cones are not considered
     """
-    def __init__(self, omega, freq=False, output_folder="./"):
+    def __init__(self, omega, freq=False, output_folder="./") -> object:
+        """
+        Initialisation of the object
+
+        :param omega: frequency list in radians
+        :param output_folder: (optional)
+        """
         self.omega = omega
         self.freq = freq
         self.output_folder = output_folder
 
-        self.layers = []
-        self.data = []
-        self.name = []
+        self.layers = []  # input layers and force
+        self.data = []  # data results
+        self.name = []  # name of the analysis
         return
 
-    def read_csv(self, layer_file):
+    def read_csv(self, layer_file) -> object:
+        """
+        Reads csv input file and parses to object structure
+
+        :param layer_file: path to the input csv file
+        :return:
+        """
         self.layers = LayeredHalfSpace.read_file(layer_file)
         self.name = os.path.splitext(os.path.split(layer_file)[-1])[0]
         return
 
-    def parse_layers(self, force, soil, name):
+    def parse_layers(self, force, soil, name) -> object:
+        """
+        Parse the data to object structure
 
+        :param force: force
+        :param soil: soil paramenters
+        :param name: name of the analysis
+        """
+        # ToDo: work in progress
         self.layers.append(force)
         for so in soil:
             self.layers.append(",".join(so))
@@ -33,7 +52,10 @@ class WolfStiffness:
         self.name = name
         return
 
-    def compute(self):
+    def compute(self) -> object:
+        """
+        computes the dynamic stiffness following the wolf model
+        """
         self.data = LayeredHalfSpace.Layers(self.layers)
 
         self.data.assign_properties()
@@ -42,8 +64,12 @@ class WolfStiffness:
         self.data.dynamic_stiffness(self.omega)
         return
 
-    def write(self):
+    def write(self, freq=False) -> object:
+        """
+        writes the results
 
+        :param freq: (optional: default False) plots the graphs in Hz instead of rad
+        """
         LayeredHalfSpace.write_output(self.output_folder, self.name,
-                                      self.data, self.omega, self.freq)
+                                      self.data, self.omega, freq)
         return
